1b16232d13da92d37572484b6eb6934e05aa2c66
SIddhant7820 <siddhantghogare@gmail.com>
2026-01-16 15:14:11 +0530

    changes in flask app

--- app.py
+++ app.py
@@ -13,32 +13,25 @@ def test():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route('/predictdata', methods=['GET', 'POST'])
-def predict_datapoint():
-    if request.method == 'POST':
-        data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
-        )
-
-        pred_df = data.get_data_as_data_frame()
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-
-        return render_template(
-            "home.html",
-            results=round(float(results[0]), 2)
-        )
-
-    # GET request
     return render_template("home.html")
+
+@app.route('/predictdata', methods=['POST'])
+def predict_datapoint():
+    data = CustomData(
+        gender=request.form['gender'],
+        race_ethnicity=request.form['ethnicity'],
+        parental_level_of_education=request.form['parental_level_of_education'],
+        lunch=request.form['lunch'],
+        test_preparation_course=request.form['test_preparation_course'],
+        reading_score=float(request.form['reading_score']),
+        writing_score=float(request.form['writing_score'])
+    )
+
+    pred_df = data.get_data_as_data_frame()
+    predict_pipeline = PredictPipeline()
+    result = round(float(predict_pipeline.predict(pred_df)[0]), 2)
+
+    return render_template("home.html", results=result)
 
     
 
