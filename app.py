485207f3b50c394285bd17ed08b5aab862521164
SIddhant7820 <siddhantghogare@gmail.com>
2026-01-16 14:47:13 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -17,12 +17,15 @@ def index():
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template("home.html")
-    else:
+
+    try:
+        print(request.form)  # DEBUG
+
         data = CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
+            race_ethnicity=request.form.get('race_ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
@@ -31,13 +34,15 @@ def predict_datapoint():
         )
 
         pred_df = data.get_data_as_data_frame()
-
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
 
-        return render_template('home.html', results=results[0])
+        return render_template("home.html", results=results[0])
+
+    except Exception as e:
+        return f"ERROR: {str(e)}"
     
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0",port=port,debug=True)    
+    app.run(host="0.0.0.0",port=port)    
