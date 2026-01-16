92b41b7b8d703b46a6d4d270061a49920c004e53
SIddhant7820 <siddhantghogare@gmail.com>
2026-01-16 14:31:46 +0530

    changes in predict_pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -13,8 +13,9 @@ class PredictPipeline:
             model_path = os.path.join("artifacts","model.pkl")
             preprocessor_path = os.path.join("artifacts","preprocessor.pkl")
             model=load_object(file_path=model_path)
-            prerocessor=load_object(file_path=preprocessor_path)
-            data_scaled=prerocessor.transform(features)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+
             preds=model.predict(data_scaled)
             return preds
 
